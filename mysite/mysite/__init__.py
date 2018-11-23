8b5736e19badf485c0bc7fe865287bab12bc4bcb
Charlie Gallentine <chgallentine@gmail.com>
2018-11-23 13:32:13 -0600

    Add pt5

---
+++ mysite/mysite/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Charlie Gallentine
+# @Date:   2018-11-23 11:58:05
+# @Last Modified by:   Charlie Gallentine
+# @Last Modified time: 2018-11-23 13:31:11
---
+++ mysite/polls/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Charlie Gallentine
+# @Date:   2018-11-23 12:00:18
+# @Last Modified by:   Charlie Gallentine
+# @Last Modified time: 2018-11-23 13:31:07

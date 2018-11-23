9c5c2026940421b6cb71bd14fabb78ff1e1113ea
Charlie Gallentine <chgallentine@gmail.com>
2018-11-23 13:12:49 -0600

    Finished part 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,16 +2,15 @@
 # @Author: Charlie Gallentine
 # @Date:   2018-11-23 12:01:38
 # @Last Modified by:   Charlie Gallentine
-# @Last Modified time: 2018-11-23 13:00:40
+# @Last Modified time: 2018-11-23 13:09:54
 from django.urls import path, include
 from . import views
 
 app_name = 'polls'
 
 urlpatterns = [
-	# Create map from view to URL
-	path('', views.index, name='index'),
-	path('<int:question_id>/', views.detail, name='detail'),
-	path('<int:question_id>/results/', views.results, name='results'),
-	path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
